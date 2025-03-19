db05baa808dac03c69478b9250184a470ea0ebf8
camb3la <59169021+camb3la@users.noreply.github.com>
2025-03-19 16:00:52 +0100

    FIX NgForOf su Footer Component

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { NgForOf } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
+  standalone: true,
+    imports: [NgForOf, RouterLink]
 })
 export class FooterComponent {
   currentYear: number = new Date().getFullYear();
