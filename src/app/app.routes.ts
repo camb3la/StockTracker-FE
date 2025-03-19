ed60ac1756980df5e578cfccb4beaa7065e738af
camb3la <59169021+camb3la@users.noreply.github.com>
2025-03-19 13:16:40 +0100

    Aggiornamento Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 
 import { LoginComponent } from './core/auth/login/login.component';
+import { RegisterComponent } from './core/auth/register/register.component';
+
 
 export const routes: Routes = [
-
-{ path: 'login', component: LoginComponent }
+{ path: '', redirectTo: 'login', pathMatch: 'full' },
+{ path: 'login', component: LoginComponent },
+{ path: 'register', component: RegisterComponent }
 
 ];
