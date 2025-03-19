dd9019cf2773727d59daca54279910430278601d
camb3la <59169021+camb3la@users.noreply.github.com>
2025-03-19 16:01:15 +0100

    Integrati Header e Footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
+  template: `
+      <app-header></app-header>
+      <router-outlet></router-outlet>
+      <app-footer></app-footer>
+    `,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
