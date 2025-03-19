4dd0ca348a98f389dc98fbdb7b5d66f47849158b
camb3la <59169021+camb3la@users.noreply.github.com>
2025-03-19 16:00:31 +0100

    FIX NgIF su Header Component

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -2,11 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../../core/services/auth.service';
 import { User } from '../../../core/models/user.model';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  standalone: true,
+    imports: [NgIf]
 })
 export class HeaderComponent implements OnInit {
   currentUser: User | null = null;
