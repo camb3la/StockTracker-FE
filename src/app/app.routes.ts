22a1d16c65f64e2c99c745d154275165ccd26b56
camb3la <59169021+camb3la@users.noreply.github.com>
2025-03-28 11:44:31 +0100

    Aggiunte le varie Route in app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,15 @@ import { Routes } from '@angular/router';
 
 import { LoginComponent } from './core/auth/login/login.component';
 import { RegisterComponent } from './core/auth/register/register.component';
-
+import { DashboardComponent } from './features/dashboard/dashboard.component';
+import { StockComponent } from './features/stock/stock.component';
+import { AuthGuard } from './core/auth/auth.guard';
 
 export const routes: Routes = [
-{ path: '', redirectTo: 'login', pathMatch: 'full' },
-{ path: 'login', component: LoginComponent },
-{ path: 'register', component: RegisterComponent }
-
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: 'stock', component: StockComponent, canActivate: [AuthGuard] },
+  { path: '**', redirectTo: 'dashboard' }
 ];
