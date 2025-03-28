67dd0af04d793acfb36d0ce4e85ac3403c805e4b
camb3la <59169021+camb3la@users.noreply.github.com>
2025-03-28 11:44:58 +0100

    Aggiunto il servizio JWT in app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,19 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import { HttpClientModule, provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 
 import { routes } from './app.routes';
+import { jwtInterceptor } from './core/auth/jwt.Interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes) ,
-    provideHttpClient()
+    provideRouter(routes),
+    provideHttpClient(
+      withFetch(),
+      withInterceptors([jwtInterceptor])
+    ),
+    importProvidersFrom(HttpClientModule),
+    provideCharts(withDefaultRegisterables())
   ]
 };
